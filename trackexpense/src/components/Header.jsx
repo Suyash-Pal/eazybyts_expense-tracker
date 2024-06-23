a0215ed4b85e807aa76b01df592a7f791d0813ea
Suyash Pal <pal180suyash@gmail.com>
2024-06-23 15:20:19 +0530

    add this to git

---
+++ trackexpense/src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+
+  
+    return (
+        <div className='flex w-full h-14 bg-red-500
+                        items-center justify-between
+                            '>
+            <div className='min-w-[150px] pl-5 font-semibold text-2xl'>
+                <center><p>Expense Tracker</p></center>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
